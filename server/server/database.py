a8b19157b1016cba38789f4c7c8e3a4430a263fd
KrisDavie <kristofer.davie@gmail.com>
2024-08-18 22:29:48 +0200

    Limit concurent connections to max number of players * 2

--- server/server/database.py
+++ server/server/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # SQLALCHEMY_DATABASE_URL = "sqlite:////data/sql_app.db"
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=512, max_overflow=100)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
